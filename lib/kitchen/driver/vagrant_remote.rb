7fd1d9caf3d724d8d5d4e100b4ebdcc20a883530
mzp <mzpppp@gmail.com>
2016-11-21 22:44:07 +0900

    :muscle: create wrapper script to manage remote vagrant

--- lib/kitchen/driver/vagrant_remote.rb
+++ lib/kitchen/driver/vagrant_remote.rb
@@ -8,6 +8,46 @@ module Kitchen
       include ShellOut
       kitchen_driver_api_version 2
       plugin_version Kitchen::Driver::VAGRANT_REMOTE_VERSION
+
+      default_config :remote_host, ""
+      default_config :remote_root, "/tmp"
+      default_config :remote_vagrant_binary, "vagrant"
+      default_config :wrapper_script, "/tmp/wrapper"
+
+      private
+
+      def init_config(config)
+        super
+
+        File.write(@config[:wrapper_script], wrapper_code(@config))
+        File.chmod(0766, @config[:wrapper_script])
+
+        @config[:vagrant_binary] = @config[:wrapper_script]
+
+        @config = LazyHash.new(@config, self)
+      end
+
+      def run_vagrant_up
+        info "run vagrant up at #{vagrant_root}"
+        dest = "#{config[:remote_root]}/#{vagrant_root}"
+
+        run("ssh #{config[:remote_host]} mkdir -p #{dest}")
+        run("rsync -az #{vagrant_root}/ #{config[:remote_host]}:#{dest}/")
+
+        super
+      end
+
+      def wrapper_code(config)
+        <<-END
+#!/bin/sh
+
+cat <<CMD | ssh #{config[:remote_host]} bash --login
+cd #{config[:remote_root]}
+cd ./$(pwd)
+#{config[:remote_vagrant_binary]} $@
+CMD
+        END
+      end
     end
   end
 end
