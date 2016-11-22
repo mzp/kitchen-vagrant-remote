2e6ebf2e123d1a3d621b6b31e0d59835f1831c6a
mzp <mzpppp@gmail.com>
2016-11-22 09:25:06 +0900

    :lipstick: split to some methods

--- lib/kitchen/driver/vagrant_remote.rb
+++ lib/kitchen/driver/vagrant_remote.rb
@@ -9,6 +9,7 @@ module Kitchen
       kitchen_driver_api_version 2
       plugin_version Kitchen::Driver::VAGRANT_REMOTE_VERSION
 
+      default_config :remote_user, "user"
       default_config :remote_host, ""
       default_config :remote_root, "/tmp"
       default_config :remote_vagrant_binary, "vagrant"
@@ -34,6 +35,9 @@ module Kitchen
         run("ssh #{config[:remote_host]} mkdir -p #{dest}")
         run("rsync -az #{vagrant_root}/ #{config[:remote_host]}:#{dest}/")
 
+      def run_vagrant_up
+        info "run vagrant up at #{vagrant_root}"
+        copy_to_remote
         super
       end
 
@@ -41,12 +45,21 @@ module Kitchen
         <<-END
 #!/bin/sh
 
-cat <<CMD | ssh #{config[:remote_host]} bash --login
+cat <<CMD | ssh #{config[:remote_user]}@#{config[:remote_host]} bash --login
 cd #{config[:remote_root]}
 cd ./$(pwd)
 #{config[:remote_vagrant_binary]} $@
 CMD
         END
+      end
+
+      def copy_to_remote
+        run("ssh #{config[:remote_user]}@#{config[:remote_host]} mkdir -p #{remote_vagrant_root}")
+        run("rsync -az #{vagrant_root}/ #{config[:remote_user]}@#{config[:remote_host]}:#{remote_vagrant_root}/")
+      end
+
+      def remote_vagrant_root
+        "#{config[:remote_root]}/#{vagrant_root}"
       end
     end
   end
